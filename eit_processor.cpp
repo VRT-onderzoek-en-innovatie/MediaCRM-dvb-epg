8d04c6b612bec9e2c1349d29893b01a4aa27e63c
Niels Laukens <niels.laukens@vrtmedialab.be>
2010-12-11 20:21:31 +0100

    Added some checks to EIT-processor

--- eit_processor.cpp
+++ eit_processor.cpp
@@ -1,6 +1,13 @@
 #include "eit_processor.hpp"
+#include "debug.h"
 #include <stdio.h>
 
 void EIT_processor::process_section(char *section, size_t nsection) {
+	if( nsection == 0 ) {
+		warn_assert(nsection != 0);
+		return;
+	}
+	assert(section != NULL);
+
 	printf("Processing section of %lu bytes\n", nsection);
 }
