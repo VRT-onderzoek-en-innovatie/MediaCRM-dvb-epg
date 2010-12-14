9193a0d7a1473f00b4eb4282709f64f2b324a719
Niels Laukens <niels.laukens@vrtmedialab.be>
2010-12-14 15:37:09 +0100

    Added channel processor

--- eit_processor.cpp
+++ eit_processor.cpp
@@ -5,22 +5,52 @@
 #include <stdint.h>
 #include <stdio.h>
 
-void EIT_processor::process_section(const unsigned char *section, size_t nsection) {
+void EIT_channel_processor::parse_segment(const unsigned char *section, size_t nsection) {
 	assert(section != NULL);
 	assert(nsection >= 14+4); // 0 events
 
 	uint8_t table_id = section[0];
 	assert( (section[1]&0x80) == 0x80 ); // section syntax indicator
 	uint16_t section_length = ((section[1]&0x0f)<<8) | section[2];
-	uint16_t service_id = (section[3]<<8) | section[4];
+	//uint16_t service_id = (section[3]<<8) | section[4];
 	uint8_t version_number = (section[5]&0x3e)>>1;
 	bool current_next_indicator = (section[5]&0x01);
 	uint8_t section_number = section[6];
 	uint8_t last_section_number = section[7];
-	uint16_t transport_srteam_id = (section[8]<<8) | section[9];
-	uint16_t original_network_id = (section[10]<<8) | section[11];
+	//uint16_t transport_stream_id = (section[8]<<8) | section[9];
+	//uint16_t original_network_id = (section[10]<<8) | section[11];
 	uint8_t segment_last_section_number = section[12];
 	uint8_t last_table_id = section[13];
+
+	const unsigned char *section_end = section+3 + section_length - 4; // "immediately following the length field" + legth - CRC32
+	section += 14;
+
+	while( section < section_end ) {
+		Event *e = new Event(&section);
+
+		delete e;
+	}
+}
+
+EIT_channel_processor::~EIT_channel_processor() {
+}
+
+void EIT_processor::process_section(const unsigned char *section, size_t nsection) {
+	assert(section != NULL);
+	assert(nsection >= 14+4); // 0 events
+
+	//uint8_t table_id = section[0];
+	assert( (section[1]&0x80) == 0x80 ); // section syntax indicator
+	uint16_t section_length = ((section[1]&0x0f)<<8) | section[2];
+	uint16_t service_id = (section[3]<<8) | section[4];
+	//uint8_t version_number = (section[5]&0x3e)>>1;
+	//bool current_next_indicator = (section[5]&0x01);
+	//uint8_t section_number = section[6];
+	//uint8_t last_section_number = section[7];
+	uint16_t transport_stream_id = (section[8]<<8) | section[9];
+	uint16_t original_network_id = (section[10]<<8) | section[11];
+	//uint8_t segment_last_section_number = section[12];
+	//uint8_t last_table_id = section[13];
 	uint32_t crc = (section[3+section_length-4] << 24)
 			| (section[3+section_length-3] << 16)
 			| (section[3+section_length-2] << 8)
@@ -35,17 +65,9 @@ void EIT_processor::process_section(const unsigned char *section, size_t nsectio
 		}
 	}
 
-	printf("EIT decode: tid:%02x-%02x sid:%04x/%04x/%04x ver:%u sec:%u-%u/%u\n",
-		table_id, last_table_id,
-		service_id, transport_srteam_id, original_network_id,
-		version_number,
-		section_number, segment_last_section_number, last_section_number);
-
-	const unsigned char *event = section + 14;
-	const unsigned char *event_end = event + section_length - 11 - 4;
-	while( event < event_end ) {
-		Event *e = new Event(&event);
-		printf("Event: %u\n", e->m_event_id);
-		delete e;
-	}
+	struct channel_id chan_id = { original_network_id, transport_stream_id, service_id };
+	std::pair< typeof(m_channels.begin()), bool > chan_insert
+		= m_channels.insert( std::pair< struct channel_id, EIT_channel_processor >
+			(chan_id, EIT_channel_processor() ) ); // insert or find
+	chan_insert.first->second.parse_segment(section, nsection);
 }
--- eit_processor.hpp
+++ eit_processor.hpp
@@ -2,9 +2,33 @@
 #define __EIT_PROCESSOR_HPP__
 
 #include "section_buffer.hpp"
+#include <stdint.h>
+#include <map>
+#include <list>
+
+class EIT_channel_processor {
+public:
+	void parse_segment(const unsigned char *section, size_t nsection);
+
+	~EIT_channel_processor();
+};
 
 class EIT_processor: public Section_Processor {
 public:
+	struct channel_id {
+		uint16_t original_network_id;
+		uint16_t transport_stream_id;
+		uint16_t service_id;
+		bool operator< (const struct channel_id &that) const {
+			if( this->original_network_id != that.original_network_id )
+				return this->original_network_id < that.original_network_id;
+			if( this->transport_stream_id != that.transport_stream_id )
+				return this->transport_stream_id < that.transport_stream_id;
+			return this->service_id < that.service_id;
+		}
+	};
+	std::map< struct channel_id, EIT_channel_processor > m_channels;
+
 	virtual void process_section(const unsigned char *section, size_t nsection);
 };
 
