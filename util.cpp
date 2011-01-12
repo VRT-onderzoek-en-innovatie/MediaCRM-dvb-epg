a06401136813188a4c70a5348efbbf1bf89ebe1b
Niels Laukens <niels.laukens@vrtmedialab.be>
2011-01-12 15:04:14 +0100

    Bugfix: MJD calculation used local timezone instead of UTC

--- util.cpp
+++ util.cpp
@@ -8,16 +8,9 @@ uint8_t bcd_decode(uint8_t in) {
 	return (in>>4)*10+(in&0x0f);
 }
 
+#define UNIX_EPOCH_IN_MJD 40587
 time_t mjd_decode(uint16_t mjd) {
-	struct tm ret;
-	
-	int ya = ((double)mjd - 15078.2)/365.25;
-	int ma = ((double)mjd - 14956.1 - floor(ya * 365.25)) / 30.6001;
-	ret.tm_mday = mjd - 14956 - floor(ya*365.25) - floor(ma*30.6001);
-	int k = (ma==14 || ma==15 ? 1 : 0);
-	ret.tm_year = ya + k;
-	ret.tm_mon = ma - 1 - k*12   -1;
-	ret.tm_hour = ret.tm_min = ret.tm_sec = 0;
-
-	return mktime(&ret);
+	assert( mjd >= UNIX_EPOCH_IN_MJD );
+	mjd -= UNIX_EPOCH_IN_MJD;
+	return mjd * 86400;
 }
