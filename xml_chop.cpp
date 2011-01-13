45523274591e0c66f1e4f59984458b7a27d93645
Niels Laukens <niels.laukens@vrtmedialab.be>
2011-01-13 10:58:03 +0100

    Finished xml_chop: it filters stdin to stdout to only provide the last root-element (and its contents)

--- xml_chop.cpp
+++ xml_chop.cpp
@@ -1,13 +1,45 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <expat.h>
+#include <string>
+
+std::string buffer;
+unsigned long depth = 0;
 
 void start(void *userData, const XML_Char *name, const XML_Char **atts) {
-	printf("Open %s\n", name);
+	if( depth == 1 ) { // new root element (inside our own dummy-root)
+		buffer.clear();
+	}
+	buffer.append("<");
+	buffer.append(name);
+	for( int i = 0; atts[i]; i+=2 ) {
+		buffer.append(" ");
+		buffer.append(atts[i]);
+		buffer.append("=\"");
+		buffer.append(atts[i+1]);
+		buffer.append("\"");
+	}
+	buffer.append(">");
+	depth++;
+}
+
+void cdatastart(void *userData) {
+	buffer.append("<![CDATA[");
+}
+
+void chardata(void *userData, const XML_Char *s, int len) {
+	buffer.append(s, len);
+}
+
+void cdataend(void *userData) {
+	buffer.append("]]>");
 }
 
 void end(void *userData, const XML_Char *name) {
-	printf("Close %s\n", name);
+	depth--;
+	buffer.append("</");
+	buffer.append(name);
+	buffer.append(">");
 }
 
 
@@ -16,6 +48,8 @@ int main(int argc, char *argv[]) {
 	XML_Parser xp = XML_ParserCreate(NULL);
 
 	XML_SetElementHandler(xp, start, end);
+	XML_SetCdataSectionHandler(xp, cdatastart, cdataend);
+	XML_SetCharacterDataHandler(xp, chardata);
 
 	XML_Parse(xp, "<dummyRootElement>", 18, 0);
 	while(1) {
@@ -33,9 +67,9 @@ int main(int argc, char *argv[]) {
 			return 1;
 		}
 		
-		if (! XML_ParseBuffer(xp, bytes_read, bytes_read == 0)) {
+		if (! XML_ParseBuffer(xp, bytes_read, 0)) {
 			/* handle parse error */
-			fprintf(stderr, "Parse error at byte %d (line %d, column %d): %s\n",
+			fprintf(stderr, "Parse error at byte %ld (line %ld, column %ld): %s\n",
 				XML_GetCurrentByteIndex(xp), XML_GetCurrentLineNumber(xp),
 				XML_GetCurrentColumnNumber(xp), XML_ErrorString( XML_GetErrorCode(xp) ) );
 			return 1;
@@ -43,6 +77,8 @@ int main(int argc, char *argv[]) {
 		if (bytes_read == 0)
 			break;
 	}
+
+	write(1, buffer.data(), buffer.length());
 
 	XML_Parse(xp, "</dummyRootElement>", 19, 1);
 
